7dddbb094516ed368dd4a4c90af04ee9b97f7246
brandondelpozo <branzzel@gmail.com>
2021-09-23 01:45:38 -0500

    viewsets added

--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import UserList, UserDetail, PostList, PostDetail # new
+from rest_framework.routers import SimpleRouter
+from .views import UserViewSet, PostViewSet # new
 
-urlpatterns = [
-    path('users/', UserList.as_view()), # new
-    path('users/<int:pk>/', UserDetail.as_view()), # new
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-]
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
+
+urlpatterns = router.urls
