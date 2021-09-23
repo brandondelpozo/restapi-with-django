8e3c210c4aea1a5e4b57403877318057772f7995
brandondelpozo <branzzel@gmail.com>
2021-09-22 23:49:22 -0500

    update serializers.py

--- blogapi/posts/serializers.py
+++ blogapi/posts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model # new
 from rest_framework import serializers
 from .models import Post
 
@@ -7,3 +8,9 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'author', 'title', 'body', 'created_at',)
         model = Post
+
+class UserSerializer(serializers.ModelSerilizer): # new
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
